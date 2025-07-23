fd157cb809ca83195bc58a353bf16e64dabeb4ff
kimliya1 <136612018+kimliya1@users.noreply.github.com>
2025-07-24 02:37:37 +0900

    Update dialog.js
    대화 추가

--- dialog.js
+++ dialog.js
@@ -53,7 +53,7 @@ function dialogList(loglist){
 
     /* 초급 회화 */
     let keyTalk =[
-        "기본/리레보다 어둡고 다크브라운레드보다는 밝은 레드는 없는걸까...",
+        "기본/리레보다 어둡고/다크브라운레드보다는/밝은 레드는 없는걸까...",
         "기본/월석도 치워야하는데, 귀찮지. 재료도 비싸고."
     ];
 
@@ -63,11 +63,11 @@ function dialogList(loglist){
 
     let keySkill = [
         "기본/격투술은 어렵지.<br>정해진대로 움직여야하니까, 별로 좋아하진 않아.",
-        "기본/역시 제일 좋아하는 건 검술이나 ",
+        "기본/역시 제일 좋아하는 건 검술이나 창술일까./마법도 편하긴 하지만 직접 움직이는 편이 좋아.",
     ];
 
     let keyParttime = [
-        "기본/잡화점 아르바이트로 쌓아둔 낚시통이 많아.<br>언제쯤 다 쓸 수 있으려나."
+        "기본/"
     ];
 
     let keyClass=[
@@ -75,8 +75,17 @@ function dialogList(loglist){
     ];
 
     /* 중급 회화 */
+    let keyGeneralstore = [
+        "기본/잡화점 아르바이트로 쌓아둔 낚시통이 많아.<br>언제쯤 다 쓸 수 있으려나."
+    ];
+
     let keySchool = [
         "기본/학교를 다녀보고 싶다는 생각은 해본 적은 있어./책을 보거나 직접 배우기만 했으니까."
+    ];
+
+    let keyFishingSkill = [
+        "기본/낚시통이 조금 더 컸으면 좋겠네.",
+        "기본/청새치를 낚은지 한참 지나서 마놀린이 괜찮을지 걱정되긴 해.",
     ];
 
     /* 고급 회화 */
@@ -85,12 +94,40 @@ function dialogList(loglist){
     ];
 
     /* 정보 메모 */
+    let keyTearsoftheSpirit = [
+        "기본/진짜 눈물이 아니라는 건 알지만 그래도 미안하다고 생각해.",
+        "먼눈/하루에 하나는 좀... 아니야.",
+    ];
+
+    let keyspiritEskel = [
+        "기본/에스켈은 상냥한 정령이야. 여태 못 챙겨준게 걸리네.", 
+        "기본/에스켈은 내 첫번째 정령이라 그런지 여기저기 자주 옮겨다녔지./한손 검이였다가 체인에 들어갔기도 했고... 대낫에서 지금은 랜스로 들어왔어.", 
+    ];
+
     let keyPresentforNao = [
-        "기본/나오에게 옷을 많이 줬는데 늘 같은 옷만 입더라고./역시 죽어봐야하나..."
+        "기본/나오에게 옷을 많이 줬는데 늘 같은 옷만 입더라고./역시 죽어봐야하나...",
+        "먼눈/나오가 좋아할만한 선물이 없을까..."
     ];
 
     let keyAlltur = [
         "미소/귀여운 애지./쿠키를 주길래 아껴놨는데 저번에 식겁하더니 새로 만들어준다더라고."
+    ];
+
+    let keyAveline = [
+        "미소/뭔가... 따르게 된단 말이지. 좋은 사람이야."
+    ];
+
+    let keyTorvish = [
+        "기본/별로 마음에 드는 사람은 아니야./가끔... 짜증나긴 해."
+    ];
+
+    let keyLlywelyn = [
+        "기본/아직 어떤 사람인지 잘 모르겠어."
+    ];
+    
+    let keyRuairi = [
+        "눈감/...별로 이야기하고 싶지않네.",
+        "기본/짜증나는 녀석이야.",
     ];
 
     /* 리스트 미지정 */
@@ -100,6 +137,12 @@ function dialogList(loglist){
         "눈감/..."
     ];
 
+    /*
+    let key = [
+        ""
+    ];
+     */
+
     switch(loglist){
         /* 초급 회화 */
         case 'keyTalk': return keyTalk; // 개인적인 이야기
@@ -108,12 +151,20 @@ function dialogList(loglist){
         case 'keyParttime': return keyParttime; // 아르바이트에 대하여
         case 'keyClass': return keyClass; // 수업과 수련에 대하여
         /* 중급 회화 */
+        case 'keyGeneralstore': return keyGeneralstore; // 잡화점
         case 'keySchool': return keySchool; // 학교
+        case 'keyFishingSkill': return keyFishingSkill; // 낚시 스킬
         /* 고급 회화 */
         case 'keyTirNaNog': return keyTirNaNog; // 티르 나 노이
         /* 정보 메모 */
+        case 'keyTearsoftheSpirit' : return keyTearsoftheSpirit // 정령의 눈물
+        case 'keyspiritEskel' : return keyspiritEskel; //에스켈
         case 'keyPresentforNao': return keyPresentforNao; // 나오에게 선물
         case 'keyAlltur': return keyAlltur; // 알터
+        case 'keyAveline': return keyAveline; // 아벨린
+        case 'keyTorvish': return keyTorvish; // 톨비쉬
+        case 'keyLlywelyn': return keyLlywelyn; // 르웰린
+        case 'keyRuairi': return keyRuairi; // 루에리
         /* 리스트 미지정 */
         default: return defaultList; // 그 외 (없는 키워드)
     }
